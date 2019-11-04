d44178d0d85392eb3355c668fd420ec7e356877f
AbhishekYadav142110 <abhishekyadav142110@gmail.com>
2019-11-05 00:18:29 +0530

    Search functionality
    version 1

--- media.net/ListElement.js
+++ media.net/ListElement.js
@@ -5,10 +5,20 @@ export default class ListElement extends React.Component{
             super(props);
             this.state = {
                 isChecked: false,
-                isDeleted: false
+                isDeleted: false,
+                searchValue: this.props.searchValue
             }
             this.handleDelete = this.handleDelete.bind(this);
             this.handleChange = this.handleChange.bind(this);
+        }
+
+        static getDerivedStateFromProps(props, state){
+            if (props.searchValue !== state.searchValue){
+                return {
+                  searchValue: props.searchValue
+                };
+              }
+            return null;  
         }
 
         handleDelete(e){
@@ -25,37 +35,43 @@ export default class ListElement extends React.Component{
         }
 
         render(){
-            let display;  
-            
-            if(this.props.isChecked && !this.state.isDeleted){
-                display = this.state.isChecked && 
-                <>
-                <tr>
+            let display;
+            let row = 
+            <tr>
+                <td>
                     <input type="checkbox" value={this.state.isChecked} onChange={this.handleChange}/>
-                </tr>
-                <tr>
+                </td>
+                <td>
                     {this.props.email}
-                </tr>
-                <tr>
+                </td>
+                <td>
                     <button onClick={this.handleDelete}>Delete</button>
-                </tr>
-            </>
+                </td>
+            </tr>
+
+            if(this.props.isChecked && !this.state.isDeleted){
+                display = this.state.isChecked && row
             }
             else{
-            display = this.state.isDeleted?null:
-            <>
-                <tr>
-                    <input type="checkbox" value={this.state.isChecked} onChange={this.handleChange}/>
-                </tr>
-                <tr>
-                    {this.props.email}
-                </tr>
-                <tr>
-                    <button onClick={this.handleDelete}>Delete</button>
-                </tr>
-            </> 
-            }
-            return(display);
+                display = this.state.isDeleted?null:row
             }
 
-}
+            return(display);
+        }
+
+         componentDidUpdate(prevProps, prevState){
+             if(this.props.searched && prevProps.searchValue!=this.props.searchValue){
+                if(this.props.email.includes(this.state.searchValue)){
+                    this.setState({
+                    isDeleted: false
+                    })
+                }
+                else{
+                    this.setState({
+                        isDeleted: true
+                    })
+                }
+            }
+        }
+
+}    
--- media.net/assgn.js
+++ media.net/assgn.js
@@ -8,11 +8,29 @@ export default class Media extends React.Component{
         this.state = {
             value: '',
             emailAddresses: [],
-            isChecked: false
+            isChecked: false,
+            valid: true,
+            searchValue: '',
+            searched: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
         this.handleCheck = this.handleCheck.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.search = this.search.bind(this);
+        this.unsearch = this.unsearch.bind(this);
+    }
+
+    search(){
+        this.setState({
+            searched: true
+        })
+    }
+
+    unsearch(){
+        this.setState({
+            searched: false
+        })
     }
 
     handleCheck(){
@@ -24,38 +42,55 @@ export default class Media extends React.Component{
 
     handleClick(e){
         let list = this.state.emailAddresses;
-        list.push(this.state.value);
-
-        this.setState({
-            value: '',
-            emailAddresses: list
-        })
+        
+        if(this.state.value.includes('@') && this.state.value.includes('.')){
+            list.push(this.state.value);
+            this.setState({
+                value: '',
+                emailAddresses: list,
+                valid: true
+            })
+        }
+        else{
+            this.setState({
+                valid: false
+            })
+        }
     }
 
     handleChange(e){
         this.setState({value: event.target.value});
     }
 
+    handleSearch(e){
+        this.setState({searchValue: event.target.value});
+    }
+
     render(){
 
         let emails = this.state.emailAddresses.map((email, index) => {
             return (
-                <ListElement isChecked={this.state.isChecked} email={email} index={index} />
+                <ListElement searched={this.state.searched} searchValue={this.state.searched && this.state.searchValue} isChecked={this.state.isChecked} email={email} index={index} />
             );
         });
 
         return (
-            <div className="container">
+            <div className="container" style={{display: "flex"}}>
                 <div className="left">
                     <div>
                         Email Address
                     </div>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" value={this.state.value} onChange={this.handleChange} style={{display:"block"}} />
+                    {!this.state.valid && <div style={{color:"red"}}>Not a valid email</div>}
                     <button onClick={this.handleClick}>
                         Add
                     </button>
                 </div>
                 <div className="right">
+                    <div>
+                        <input type="text" value={this.state.searchValue} onChange={this.handleSearch} />
+                        <button onClick={this.search}>Search</button>
+                    </div>
                     <input type="checkbox" value={this.state.isChecked} onChange={this.handleCheck}/>
                     Show enabled emails
                     <div>
@@ -72,4 +107,12 @@ export default class Media extends React.Component{
             </div>
         );
     }
+
+    componentDidUpdate(prevProps, prevState){
+        if(!this.state.searched && prevState.searchValue!=this.state.searchValue){
+            this.setState({
+                searched: false
+            })
+        }
+    }               
 }
